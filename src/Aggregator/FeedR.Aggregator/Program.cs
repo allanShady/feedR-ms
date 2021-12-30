64c0023c10478f457779ad773d0e316566a6aa48
allanShady <acamilo.ag@gmail.com>
2021-12-30 05:30:54 +0200

    chore: use transforms

--- src/Aggregator/FeedR.Aggregator/Program.cs
+++ src/Aggregator/FeedR.Aggregator/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "FeedR Aggregator");
+app.MapGet("/", async ctx => 
+{
+    await ctx.Response.WriteAsync($"FeedR Aggregator, request ID:, {ctx.Request.Headers["x-request-id"]}");
+}
+);
 
-app.Run();
+app.Run(); 
