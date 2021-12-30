3ac951c7da355db5b92bf8e46153c702ca847e9c
allanShady <acamilo.ag@gmail.com>
2021-12-30 04:51:47 +0200

    chore: setup API Gateway with Yarp

--- src/Gateway/FeedR.Gateway/Program.cs
+++ src/Gateway/FeedR.Gateway/Program.cs
@@ -1,8 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("yarp"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "FeedR Gateway");
+app.MapReverseProxy();
 
 app.Run();
