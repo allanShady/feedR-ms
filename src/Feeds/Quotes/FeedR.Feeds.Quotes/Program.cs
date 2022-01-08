deffc676681dd358e09e449b35760f3dcbab774e
allanShady <acamilo.ag@gmail.com>
2022-01-08 10:03:07 +0200

    wip register background service

--- src/Feeds/Quotes/FeedR.Feeds.Quotes/Program.cs
+++ src/Feeds/Quotes/FeedR.Feeds.Quotes/Program.cs
@@ -1,4 +1,10 @@
+using FeedR.Feeds.Quotes.Pricing.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddSingleton<IPricingGenerator, PricingGenerator>()
+    .AddHostedService<PricingBackgroundService>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "FeedR Quotes feed");
