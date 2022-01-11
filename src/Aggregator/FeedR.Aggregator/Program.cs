bd9bb3150045386b01aa417de5d29a20a1fb055d
allanShady <acamilo.ag@gmail.com>
2022-01-11 10:31:28 +0200

    wip: add redis (stack exchange)

--- src/Aggregator/FeedR.Aggregator/Program.cs
+++ src/Aggregator/FeedR.Aggregator/Program.cs
@@ -1,4 +1,8 @@
+using FeedR.Shared.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRedis(builder.Configuration);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "FeedR Aggregator");
