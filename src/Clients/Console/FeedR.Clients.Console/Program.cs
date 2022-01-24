8d41814aa46da15a233d916ca8e91eab29333dfd
allanShady <acamilo.ag@gmail.com>
2022-01-24 12:46:44 +0200

    wip: add gRPC console client

---
+++ src/Clients/Console/FeedR.Clients.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using FeedR.Feeds.Quotes.Pricing;
+
+using Grpc.Net.Client;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5041");
+var client = new PricingFeed.PricingFeedClient(channel);
+
+Console.WriteLine("Press any key to get symbols ...");
+Console.ReadKey();
+
+var symbolsResponse = await client.GetSymbolsAsync(new GetSymbolsRequest());
+foreach (var symbol in symbolsResponse.Symbols)
+    Console.WriteLine(symbol);
