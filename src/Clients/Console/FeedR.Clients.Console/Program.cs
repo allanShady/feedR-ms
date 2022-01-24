82da3dc17b8987e008a869e3a98ced5a52347d1a
allanShady <acamilo.ag@gmail.com>
2022-01-24 16:48:09 +0200

    feat: implement client - subscriber for pricing listener

--- src/Clients/Console/FeedR.Clients.Console/Program.cs
+++ src/Clients/Console/FeedR.Clients.Console/Program.cs
@@ -1,4 +1,4 @@
-﻿using FeedR.Feeds.Quotes.Pricing;
+﻿using FeedR.Clients.Console;
 
 using Grpc.Net.Client;
 // See https://aka.ms/new-console-template for more information
@@ -7,9 +7,29 @@ Console.WriteLine("Hello, World!");
 using var channel = GrpcChannel.ForAddress("http://localhost:5041");
 var client = new PricingFeed.PricingFeedClient(channel);
 
-Console.WriteLine("Press any key to get symbols ...");
+Console.WriteLine("Press any key to get symbols ...   ");
 Console.ReadKey();
 
 var symbolsResponse = await client.GetSymbolsAsync(new GetSymbolsRequest());
 foreach (var symbol in symbolsResponse.Symbols)
     Console.WriteLine(symbol);
+
+Console.WriteLine("provide a symbol (or leave empty): ");
+var providedSymbol = Console.ReadLine()?.ToUpperInvariant();
+
+if (!string.IsNullOrWhiteSpace(providedSymbol) && !symbolsResponse.Symbols.Contains(providedSymbol))
+{
+    Console.WriteLine($"Invalid provided symbol: . {providedSymbol}");
+    return;
+}
+
+var pricingStream = client.SubscribePricing(new PricingRequest
+{
+    Symbol = providedSymbol
+});
+
+while (await pricingStream.ResponseStream.MoveNext(CancellationToken.None))
+{
+    var current = pricingStream.ResponseStream.Current;
+    Console.WriteLine($"{DateTimeOffset.FromUnixTimeMilliseconds(current.Timestamp):T} -> {current.Symbol} = {current.Value / 100M}");
+}
