4b377aa5a936e4eb092b55504a051c384fb8f8bc
allanShady <acamilo.ag@gmail.com>
2022-01-30 15:26:53 +0200

    rector: add polly to handle HTTP error

--- src/Shared/FeedR.Shared/HTTP/Extensions.cs
+++ src/Shared/FeedR.Shared/HTTP/Extensions.cs
@@ -1,4 +1,7 @@
+using System.Net;
 using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Extensions.Http;
 
 namespace FeedR.Shared.HTTP;
 
@@ -7,7 +10,14 @@ public static class Extensions
     public static IServiceCollection AddHttpApiClient<TInterface, TClient>(this IServiceCollection services)
         where TInterface : class where TClient : class, TInterface
     {
-        services.AddSingleton<TInterface, TClient>();
+        services.AddHttpClient<TInterface, TClient>()
+            .AddPolicyHandler(GetPolicy());
         return services;
     }
+
+    static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+        => HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .OrResult(msg => msg.StatusCode == HttpStatusCode.BadRequest)
+            .WaitAndRetryAsync(3, retry => TimeSpan.FromSeconds(Math.Pow(2, retry)));
 }
