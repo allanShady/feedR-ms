84d7da40c590e7c769d5822dfe0ae994354d7b22
allanShady <acamilo.ag@gmail.com>
2022-01-30 14:30:34 +0200

    feat: implement weather background service to stream weather data

--- src/Feeds/Weather/FeedR.Feeds.Weather/Services/WeatherBackgroundService.cs
+++ src/Feeds/Weather/FeedR.Feeds.Weather/Services/WeatherBackgroundService.cs
@@ -1,20 +1,28 @@
+using FeedR.Shared.Streaming;
+
 namespace FeedR.Feeds.Weather.Services;
 
 internal sealed class WeatherBackgroundService : BackgroundService
 {
     private readonly IWeatherFeed _weatherFeed;
+    private readonly IStreamPublisher _streamPublisher;
     private readonly ILogger<WeatherBackgroundService> _logger;
 
-    public WeatherBackgroundService(IWeatherFeed weatherFeed, ILogger<WeatherBackgroundService> logger)
+    public WeatherBackgroundService(IWeatherFeed weatherFeed, IStreamPublisher streamPublisher, ILogger<WeatherBackgroundService> logger)
     {
         _weatherFeed = weatherFeed;
+        _streamPublisher = streamPublisher;
         _logger = logger;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         await foreach (var weather in _weatherFeed.SubscribeAsync("Nampula", stoppingToken))
+        {
             _logger.LogInformation($"{weather.Location}:  {weather.Temperature} C, {weather.Humidity} %," +
             $"{weather.Wind} km/h [{weather.Condition}]");
+
+            await _streamPublisher.PublihsAsync("weather", weather);
+        }
     }
 }
