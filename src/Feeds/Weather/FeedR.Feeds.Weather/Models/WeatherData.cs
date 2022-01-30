eba15953e345ee48f1858dbbcbdc95c90d97a151
allanShady <acamilo.ag@gmail.com>
2022-01-30 13:12:01 +0200

    wip: implement weather feed service

---
+++ src/Feeds/Weather/FeedR.Feeds.Weather/Models/WeatherData.cs
@@ -0,0 +1,2 @@
+namespace FeedR.Feeds.Weather.Models;
+internal record WeatherData(string Location, double Temperature, double Humidity, double Wind, string Condition);
--- src/Feeds/Weather/FeedR.Feeds.Weather/Program.cs
+++ src/Feeds/Weather/FeedR.Feeds.Weather/Program.cs
@@ -1,4 +1,12 @@
+using FeedR.Feeds.Weather.Services;
+using FeedR.Shared.HTTP;
+
 var builder = WebApplication.CreateBuilder(args);
+builder
+    .Services
+    .AddHttpClient()
+    .AddHttpApiClient<IWeatherFeed, WeatherFeed>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "FeedR Weather feed");
