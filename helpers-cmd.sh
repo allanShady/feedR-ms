676126f23ab11122529dc9c08d8d81c49a781228
allanShady <acamilo.ag@gmail.com>
2022-03-08 07:29:26 +0200

    chore: sub. deprecated flag -p with --project

--- helpers-cmd.sh
+++ helpers-cmd.sh
@@ -1,16 +1,16 @@
 #!bin/bash     
      
-dotnet run -p src/Aggregator/FeedR.Aggregator/FeedR.Aggregator.csproj 
-dotnet run -p src/Aggregator/FeedR.Aggregator/FeedR.Aggregator.csproj --urls "http://*:5011"
-dotnet run -p src/Aggregator/FeedR.Aggregator/FeedR.Aggregator.csproj --urls "http://*:5012"
+dotnet run --project src/Aggregator/FeedR.Aggregator/FeedR.Aggregator.csproj 
+dotnet run --project src/Aggregator/FeedR.Aggregator/FeedR.Aggregator.csproj --urls "http://*:5011"
+dotnet run --project src/Aggregator/FeedR.Aggregator/FeedR.Aggregator.csproj --urls "http://*:5012"
       
-dotnet run -p src/Notifier/FeedR.Notifier/FeedR.Notifier.csproj 
+dotnet run --project src/Notifier/FeedR.Notifier/FeedR.Notifier.csproj 
 
-dotnet run -p src/Gateway/FeedR.Gateway/FeedR.Gateway.csproj 
+dotnet run --project src/Gateway/FeedR.Gateway/FeedR.Gateway.csproj 
 
-dotnet run -p src/Feeds/News/FeedR.Feeds.News/FeedR.Feeds.News.csproj  
-dotnet run -p src/Feeds/Weather/FeedR.Feeds.Weather/FeedR.Feeds.Weather.csproj  
-dotnet run -p src/Feeds/Quotes/FeedR.Feeds.Quotes/FeedR.Feeds.Quotes.csproj 
+dotnet run --project src/Feeds/News/FeedR.Feeds.News/FeedR.Feeds.News.csproj  
+dotnet run --project src/Feeds/Weather/FeedR.Feeds.Weather/FeedR.Feeds.Weather.csproj  
+dotnet run --project src/Feeds/Quotes/FeedR.Feeds.Quotes/FeedR.Feeds.Quotes.csproj 
 
 #Create console sample client for GRPC endpoint
 dotnet new console -n FeedR.Clients.Console -o src/Clients/Console/FeedR.Clients.Console 
